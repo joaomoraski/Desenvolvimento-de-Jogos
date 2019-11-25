b0f52afb140ff90957f78a30a22baf73a86791ce
joaomoraski <44870236+joaomoraski@users.noreply.github.com>
2019-11-25 01:03:23 -0300

    Add files via upload

---
+++ Game_02/src/com/moraski/entities/Ammo.java
@@ -0,0 +1,13 @@
+package com.moraski.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Ammo extends Entity{
+
+	public Ammo(double x, double y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+
+	}
+
+	
+}
---
+++ Game_02/src/com/moraski/entities/LifePack.java
@@ -0,0 +1,12 @@
+package com.moraski.entities;
+
+import java.awt.image.BufferedImage;
+
+public class LifePack extends Entity{
+
+	public LifePack(double x, double y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+		// TODO Auto-generated constructor stub
+	}
+
+}
